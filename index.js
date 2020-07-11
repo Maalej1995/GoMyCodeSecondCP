13d8954d8f26adee73226c6c8fee371ded86124c
Maalej1995 <59769641+Maalej1995@users.noreply.github.com>
2020-07-11 16:48:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-let pets = [
+const pets = [
   { name: "Max", type: "dog", bornOn: 2018 },
   { name: "Angel", type: "cat", bornOn: 2015 },
   { name: "Jasper", type: "dog", bornOn: 2016 }
 ];
 
-const petsWithAge = pets.map(pet => pet.age = new Date().getFullYear() - pet.bornOn);
+const petAge = pet => new Date().getFullYear() - pet.bornOn
+
+const petsWithAge = pets.map(pet => ({...pet, age: petAge(pet)}))
 console.log(petsWithAge);
 
 const dogs = pets.filter(pet => pet.type === "dog");
 console.log(dogs);
 
-const jasper = pets.find(pet => pet.name === "Jasper");
+const jasper = petsWithAge.find(pet => pet.name === "Jasper");
 console.log(`Jasper is ${jasper.age} years old`);
